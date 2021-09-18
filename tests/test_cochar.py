eeaedb8b1d698f232180c5b0fe3eb6a661a80a98
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2021-09-18 15:26:51 +0200

    test update

--- tests/test_cochar.py
+++ tests/test_cochar.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 class TestCharacter(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.global_char = Character()
 
     @classmethod
     def tearDownClass(cls):
@@ -23,6 +23,274 @@ class TestCharacter(unittest.TestCase):
         char.age = 21
         self.assertEqual(char.age, 21)
 
+    def test_strength_normal(self):
+        self.global_char.strength = 0
+
+    def test_strength_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.strength = -1
+
+    def test_strength_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.strength = "a"
+
+    def test_condition_normal(self):
+        self.global_char.condition = 0
+
+    def test_condition_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.condition = -1
+
+    def test_condition_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.condition = "a"
+
+    def test_size_normal(self):
+        self.global_char.size = 0
+
+    def test_size_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.size = -1
+
+    def test_size_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.size = "a"
+
+    def test_dexterity_normal(self):
+        self.global_char.dexterity = 0
+
+    def test_dexterity_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.dexterity = -1
+
+    def test_dexterity_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.dexterity = "a"
+
+    def test_apperance_normal(self):
+        self.global_char.apperance = 0
+
+    def test_apperance_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.apperance = -1
+
+    def test_apperance_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.apperance = "a"
+
+    def test_edducation_normal(self):
+        self.global_char.edducation = 0
+
+    def test_edducation_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.edducation = -1
+
+    def test_edducation_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.edducation = "a"
+
+    def test_intelligence_normal(self):
+        self.global_char.intelligence = 0
+
+    def test_intelligence_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.intelligence = -1
+
+    def test_intelligence_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.intelligence = "a"
+
+    def test_power_normal(self):
+        self.global_char.power = 0
+
+    def test_power_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.power = -1
+
+    def test_power_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.power = "a"
+    
+    def test_sanity_points_normal(self):
+        self.global_char.sanity_points = 0
+
+    def test_sanity_points_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.sanity_points = -1
+
+    def test_sanity_points_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.sanity_points = "a"
+
+    def test_magic_points_normal(self):
+        self.global_char.magic_points = 0
+
+    def test_magic_points_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.magic_points = -1
+
+    def test_magic_points_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.magic_points = "a"
+    
+    def test_hit_points_normal(self):
+        self.global_char.hit_points = 0
+
+    def test_hit_points_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.hit_points = -1
+
+    def test_hit_points_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.hit_points = "a"
+
+    def test_luck_normal(self):
+        self.global_char.luck = 0
+
+    def test_luck_below_range(self):
+        with self.assertRaises(ValueError):
+            self.global_char.luck = -1
+
+    def test_luck_not_integer(self):
+        with self.assertRaises(ValueError):
+            self.global_char.luck = "a"
+
+#### OCCUPATIONS ####
+
+    def test_occupation_antiquarian(self):
+        occupation = "antiquarian"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_artist(self):
+        occupation = "artist"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_athlete(self):
+        occupation = "athlete"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_author(self):
+        occupation = "author"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_clergy(self):
+        occupation = "clergy"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_criminal(self):
+        occupation = "criminal"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_dilettante(self):
+        occupation = "dilettante"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_doctor_of_medicine(self):
+        occupation = "doctor of medicine"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_drifter(self):
+        occupation = "drifter"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_engineer(self):
+        occupation = "engineer"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_entertainer(self):
+        occupation = "entertainer"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_farmer(self):
+        occupation = "farmer"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_journalist(self):
+        occupation = "journalist"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_lawyer(self):
+        occupation = "lawyer"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_librarian(self):
+        occupation = "librarian"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_military_officer(self):
+        occupation = "military officer"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_missionary(self):
+        occupation = "missionary"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_musician(self):
+        occupation = "musician"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_parapsychologist(self):
+        occupation = "parapsychologist"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_pilot(self):
+        occupation = "pilot"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_police_detective(self):
+        occupation = "police detective"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_police_officer(self):
+        occupation = "police officer"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_private_investigator(self):
+        occupation = "private investigator"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_professor(self):
+        occupation = "professor"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_soldier(self):
+        occupation = "soldier"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_tribe_member(self):
+        occupation = "tribe member"
+        c = Character(occupation=occupation)
+        self.assertEqual(c.occupation, occupation)
+
+    def test_occupation_zealot(self):
+        occupation = "zealot"
+        c = Character(occupation=occupation) 
+        self.assertEqual(c.occupation, occupation)
 
 if __name__ == "__main__":
     unittest.main()
