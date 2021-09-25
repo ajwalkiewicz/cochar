612eb6fa9f923d5c398ba6e671686a1719d778ae
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2021-09-25 18:02:16 +0200

    doc update to version 0.0.8

--- setup.py
+++ setup.py
@@ -21,9 +21,11 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         'Operating System :: OS Independent',
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        # "Programming Language :: Python :: 3.6",
+        # "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=["cochar"],
     include_package_data=True,
