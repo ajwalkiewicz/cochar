03ef260cd55977e704d8447bfb88345f5b44bc70
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2021-09-25 23:18:22 +0200

    doc update to version 0.0.9

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Adam Walkiewicz'
 author = 'Adam Walkiewicz'
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.8"
+release = "0.0.9"
 
 
 # -- General configuration ---------------------------------------------------
